f1368706e8820ec81d8e19446f8d175e2aeb29c8
Unknown <myrell-9@outlook.com>
2017-06-13 11:42:12 +0200

    Createtaskparent fix

--- inc/createtaskparent.php
+++ inc/createtaskparent.php
@@ -4,7 +4,7 @@
 	$connect = connectToDB();
 
 	if ($_POST['createbutton']) {
-		$query = "INSERT INTO `lineup`.`task` (`taskID`, `userID`, `taskName`, `taskDescription`, `taskStatus`, `taskReward`, `deadline`) VALUES (NULL, '" . $_SESSION['userID'] ."', '" . $_POST['taskname'] . "' , '" . $_POST['taskdescription'] . "', 'To Do', NULL, NULL)";
+		$query = "INSERT INTO `lineup`.`task` (`taskID`, `userID`, `taskName`, `taskDescription`, `taskStatus`, `taskReward`, `deadline`) VALUES (NULL, '" . $_SESSION['parentchildID'] ."', '" . $_POST['taskname'] . "' , '" . $_POST['taskdescription'] . "', 'To Do', '" . $_POST['taskreward'] . "' , '" . $_POST['taskdeadline'] . "')";
 	}
 
 	mysqli_query($connect, $query);
