8aff5c7e6c286d161d628b1e2651afefcea5a53c
Unknown <myrell-9@outlook.com>
2017-06-23 13:17:10 +0200

    LineUp 2.0

--- about.php
+++ about.php
@@ -22,7 +22,7 @@
 	<div id="mainLogo"><img src="img/Logo.png"></div>
 		<div id="aboutcontainer">
 			<h2>About</h2>
-			<p>*insert about text here*</p>
+			<p>LineUp is an online webapplication that helps parents get an insight into their kids' task's and progression. It was created by Myrell Richardson and Gijs Verdonschot, both first year IT-student's at the ROC ter AA in Helmond, The Netherlands.</p>
 			<button id="aboutBackButton" onclick="location.href = 'inc/redirect.php'"><h1>Go Back<h1></button>
 		</div>
 	</body>
